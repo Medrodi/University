3b1822d66967aa164a4a14f29ed8af9d7e58884f
Medrodi <danek25777@gamil.com>
2023-11-22 23:49:07 +0300

    add new

---
+++ Practice_Tasks/practice_7/viewer.py
@@ -0,0 +1,14 @@
+from get_data import *
+
+def show():
+    print('Добро пожаловать')
+    print('Выберите необходимую функцию\n'
+          '1) получить список книг со словом python\n'
+          '2) получить список с суммой * цену')
+    c = input('Ваш выбор: ')
+    if c == '1':
+        get_books_python(get_data('books.csv'))
+    elif c == '2':
+        get_total(get_data('books.csv'))
+    else:
+        print('Ошибка данных')
