b215b6bb547349e50c4e8a7c5d7d30bbffcf62c5
Medrodi <danek25777@gamil.com>
2023-12-22 07:56:57 +0300

    practice_5

--- Practice_Tasks/practice_5/file_work.py
+++ Practice_Tasks/practice_5/file_work.py
@@ -1,14 +1,36 @@
+
 def read_file(file):
     f = open(file, mode='r', encoding='utf-8')
     count_numbers = int(f.readline())
     numbers = []
     numbers_list = list(f.read().splitlines())
+    int_numbers = []
+    for i in range(numbers):
+        int_numbers.append(int(i))
+
     print(numbers_list)
 
 
 def Try():
     try:
-        read_file(input('Введите название файла: '))
+        file_name = input('Введите название файла: ')
+        read_file(file_name)
+
+    except TypeError:
+         print("Проверь тип данных")
+
+    except FileNotFoundError:
+        print("Данного файла не существует")
+
+    except OSError:
+        print("Ошибка операционной системы")
+
+    except ValueError:
+        print("Проверь символы в файле")
 
     except:
-        print('Данного файла не существует') #единственная ошибка, которая может произойти - это неправильно введённое имя файла
+        print("Произошла непредвиденная ошибка!")
+
+
+if __name__ == "__main__":
+    Try()
